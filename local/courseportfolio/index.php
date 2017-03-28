70beab56e1c812f571aff54225f4c37b9b7c25d7
Nguyen Minh Tien <tien.ngm3@gmail.com>
2017-03-28 15:18:15 +0700

    complate code + common function

---
+++ local/courseportfolio/index.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * import resource to course
+ *
+ * @package    local
+ * @subpackage courseportfolio
+ * @author     VERSION2, INC.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  2017 (C) VERSION2, INC.
+ */
+require('../../config.php');
+require_once('lib.php');
+
+$importtype   = optional_param('import_type', false, PARAM_TEXT);
+
+switch ($importtype) {
+    case 'folders':
+        break;
+
+    case 'folders_file':
+        break;
+
+    case 'topics_file':
+        break;
+
+    default:
+        print_error('unknowaction - import false');
+        break;
+}
---
+++ local/courseportfolio/lang/en/local_courseportfolio.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'courseportfolio', language 'en', branch 'MOODLE_28_STABLE'
+ *
+ * @package    local
+ * @subpackage courseportfolio
+ * @author     VERSION2, INC.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  2017 (C) VERSION2, INC.
+ */
+
+$string['courseportfolio'] = 'Class support import';
+$string['setting'] = 'Setting';
